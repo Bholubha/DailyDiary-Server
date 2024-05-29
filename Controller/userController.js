57f41c27c7573e6cadb6f9369600c9fe81262a7d
Bhautik Vala <bhautiksinhvala333@gmail.com>
2024-05-29 21:48:47 +0530

    feature of resetpassword

--- Controller/userController.js
+++ Controller/userController.js
@@ -4,65 +4,96 @@ const User = require('../models/userModel');
 
 
 
-const registerUser   = asyncHandler(async (req,res)=>{
-   const {username,email , password} = req.body;
-   if(!username || !password){
-    res.status(400);
-    throw new Error("All Fields are mendatory");
-}
-
-const availableUser = await User.findOne({email});
-
-if (availableUser){
-    res.status(400).send("User already existed");
-   exit(0);
-}
-
-console.log(req.body);
-const user = await User.create({
-    username,
-    email,
-  password
-});
-
-if(user){
-    res.json({
-        _id : user.id,
-        email : user.email,
-    })
-}else{
-    res.status(400);
-    throw new Error("We Can't registered You ! Sorry");
-}
-     
-});
- 
-
-
-
-const loginUser = asyncHandler(async (req,res)=>{
-    console.log("in login")
-    const { email , password} = req.body;
-    console.log(email)
-    console.log(password)
-    if(!email || !password){
+const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !password) {
         res.status(400);
-      throw new Error("All Fields are mandatory");
+        throw new Error("All Fields are mendatory");
     }
 
-    const user = await User.findOne({email});   
-   if(user && user.password===password){
-        console.log("verify")
-       res.json({verification:true});
-      }else{
-        console.log("Not verify")
-       res.json({verification:false});
-      
-   }
+    const availableUser = await User.findOne({ email });
 
-   
+    if (availableUser) {
+        res.status(400).send("User already existed");
+        exit(0);
+    }
+
+    console.log(req.body);
+    const user = await User.create({
+        username,
+        email,
+        password
+    });
+
+    if (user) {
+        res.json({
+            _id: user.id,
+            email: user.email,
+        })
+    } else {
+        res.status(400);
+        throw new Error("We Can't registered You ! Sorry");
+    }
+
 });
 
 
 
-module.exports = {registerUser,loginUser};
+
+const loginUser = asyncHandler(async (req, res) => {
+    console.log("in login")
+    const { email, password } = req.body;
+    console.log(email)
+    console.log(password)
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All Fields are mandatory");
+    }
+
+    const user = await User.findOne({ email });
+    if (user && user.password === password) {
+        console.log("verify")
+        res.json({ verification: true });
+    } else {
+        console.log("Not verify")
+        res.json({ verification: false });
+
+    }
+
+
+});
+
+
+
+const sendUsername = asyncHandler(async (req, res) => {
+
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+        res.json({ valid: true, username: user.username })
+    } else {
+        res.json({ valid: false })
+    }
+
+})
+
+const deleteUser = asyncHandler(async (req,res)=>{
+    const { email } = req.body;
+
+  
+        const result = await User.deleteOne({ email: email });
+
+        if(result){
+            res.send("okay")
+        }else{
+            res.send("Not Okay")
+        }
+      
+     
+    
+})
+
+
+module.exports = { registerUser, loginUser, sendUsername, deleteUser };
--- routes/User.js
+++ routes/User.js
@@ -1,10 +1,17 @@
 const express =  require("express")
 const router = express.Router();
-const {registerUser,loginUser} = require('../Controller/userController')
+const {registerUser,loginUser,sendUsername,deleteUser} = require('../Controller/userController')
 
 
 router.route("/register").post(registerUser)
 
 router.route("/login").post(loginUser);
+
+router.route("/senduser").post(sendUsername);
+
+router.route("/deleteuser").post(deleteUser);
+
+
+
 
 module.exports = router;
